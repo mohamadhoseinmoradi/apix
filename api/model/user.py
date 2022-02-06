570b0dd19c5ebf285a73e6a4c9d8cd01e0cc647d
mohamadmoradi <9889mhm@gmail.com>
2022-02-06 15:33:29 +0100

    add model

--- api/model/user.py
+++ api/model/user.py
@@ -1,4 +1,7 @@
 from api import db
 
 class User(db.Model):
-    
+
+    id = db.Column(db.String(64), primary_key=True, default=)
+    username = db.Column(db.String(128), unique=True, index=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
